c36c6e141821d24dfb60caa96e377a319891e679
Patrick Levy <mrpatricklevy@gmail.com>
2019-12-09 22:54:07 -0600

    Add site description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Harding Robotics Team`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `This is the homepage for the Harding Robotics Team - Saint Paul, MN.`,
     author: `@gatsbyjs`,
   },
   plugins: [
